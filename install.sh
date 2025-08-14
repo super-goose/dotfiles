d90130c5db7751a342d9a798eb762557ec3c25d3
Jay <theworldneedssupergoose@gmail.com>
2025-08-14 11:30:56 -0500

    use nano for git editor

--- install.sh
+++ install.sh
@@ -32,3 +32,5 @@ cd $FONT_DIR
 #ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 #git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 #ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+git config --global core.editor "nano"
